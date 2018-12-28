d0337483ee4e020080eda3450b81dace44153a95
joseantoniolandetemunoz <joseantoniolandetemunoz@192.168.1.140>
2018-12-28 21:37:53 +0100

    added Logger, DataSource and properties file to set up mysql connection

--- src/main/java/com/LandetesTest/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/LandetesTest/springsecurity/demo/config/DemoAppConfig.java
@@ -1,8 +1,15 @@
 package com.LandetesTest.springsecurity.demo.config;
 
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,7 +17,17 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.LandetesTest")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+	// variables que contendran propiedades
+
+	@Autowired
+	private Environment env;
+
+	// añadido Logger:
+
+	private Logger logger = Logger.getLogger(getClass().getName());
 
 	// este bean es equivalente a cuando haciamos en xml el :
 
@@ -31,4 +48,12 @@ public class DemoAppConfig {
 		return viewResolver;
 	}
 
+	// bean para el Datasource
+
+	@Bean
+	public DataSource securityDataSource() {
+
+		return null;
+
+	}
 }
